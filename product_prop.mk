1738006a2601cc9f3423a6d780eb9b52b2f5c4c8
merser2005 <merser2005@gmail.com>
2020-08-24 00:10:11 +0000

    jd2019: new dalvik values

--- product_prop.mk
+++ product_prop.mk
@@ -51,3 +51,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.disable_ubwc=0 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno
+
+#Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=16m
